a3ed60fca8667d9d25c830fbc6a4a5bd198796f9
Aye Thet Su Khaing <91178222+AyeThet1411@users.noreply.github.com>
2021-11-27 14:25:18 +0630

    rock-paper-scissor

--- script.js
+++ script.js
@@ -1,25 +1,66 @@
 'use strict';
-const form=document.querySelector('form');
-let feet=document.querySelector('#feet');
-let inches=document.querySelector('#inches');
-let results=document.querySelector('#results');
+const score=document.querySelector('.score');
+const message=document.querySelector('.message');
+const buttons=document.querySelectorAll('button');
+let winnerScores=[0,0];
 
-form.addEventListener('submit',function(e){
-    e.preventDefault();
+buttons.forEach(function(button){
+    button.addEventListener('click',function(e){
+        let playerSelection=e.target.innerText;
 
-    feet=parseInt(feet.value);
-    inches=parseInt(inches.value);
+        let computerSelection=Math.round(Math.random()*8);
+        if(computerSelection < 3){
+            computerSelection='Rock';
+        }else if(computerSelection < 6){
+            computerSelection='Paper';
+        }else{
+            computerSelection='Scissors';
+        }
+        //compare and retrun result
+        let result=checkWinner(playerSelection,computerSelection);
 
-    if(isNaN(feet) || isNaN(inches)){
-        results.textContent="Please enter a valid number";
-    }else if(feet < 0){
-        results.textContent="Please enter feet greater than 0";
-    }else if(inches <0 ||  inches >=12){
-        results.textContent="Please enter an inch between 0 and 12";
-    }else{
-        const convertCM=Math.round((feet*30.48)+(inches*2.54));
-        results.textContent=`${convertCM}cm`;
-        document.querySelector('#feet')=' ';
-        document.querySelector('#inches')=' ';
-    }
+        //output score
+        if(result === 'player'){
+            result+='wins!';
+            winnerScores[0]++;
+        }
+        if(result === 'computer'){
+            result+='wins!';
+            winnerScores[1]++;
+        }
+        if(result === 'Draw'){
+            result +="It's tie!"
+        }
+
+        score.innerHTML=`Player: [ ${winnerScores[0]}] &nbsp;Computer: [${winnerScores[1]}];`
+        message.innerHTML=`Player: <mark> ${playerSelection} </mark> &nbsp;
+                            Computer: <mark>${computerSelection}</mark><br><br><strong> ${result}</strong>`;
+
+    })
 })
+function checkWinner(player,computer){
+    if(player === computer){
+        return 'Draw';
+    }
+    if(player === 'Rock'){
+        if(computer === 'Paper'){
+            return 'computer';
+        }else{
+            return 'player';
+        }
+    }
+    if(player === 'Paper'){
+        if(computer === 'Scissors'){
+            return 'computer';
+        }else{
+            return 'player';
+        }
+    }
+    if(player === 'Scissors'){
+        if(computer === 'Rock'){
+            return 'computer';
+        }else{
+            return 'player';
+        }
+    }
+}
